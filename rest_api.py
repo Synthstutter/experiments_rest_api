d9681c50a88f07e08470bdaf7af35c46a512d521
Synthstutter <npalermo10@gmail.com>
2015-11-03 18:07:12 -0500

    comments to help with access to api

--- rest_api.py
+++ rest_api.py
@@ -2,12 +2,15 @@ from flask import Flask, request
 import flask.ext.sqlalchemy
 import flask.ext.restless
 
-    
+
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///experiments.db'
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
 db.drop_all()
+
+# the rest api will show up under localhost:5000/api
+# the class names below are used as the request api directory.  For example: if the class is Circ(db.Model) you can request the database at localhost:5000/api/circ
 
 class Circ(db.Model):
     id= db.Column(db.Integer, primary_key=True)
