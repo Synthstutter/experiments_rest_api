5388ee49109d56c8471471706832efab6f34b750
Nicholas Palermo <npalermo10@gmail.com>
2016-08-27 18:35:11 -0400

    pi rest-api working

--- rest_api.py
+++ rest_api.py
@@ -1,33 +1,25 @@
 from flask import Flask, request
-import flask.ext.sqlalchemy
-import flask.ext.restless
+import flask_sqlalchemy
+import flask_restless
 
-
-app = flask.Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///experiments.db'
-db = flask.ext.sqlalchemy.SQLAlchemy(app)
-db.drop_all()
-
+def run_api(database_name):
+    app = Flask(__name__)
+    app.config['DEBUG'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}.db'.format(database_name)
+    db = flask_sqlalchemy.SQLAlchemy(app)
+    db.drop_all()
 # the rest api will show up under localhost:5000/api
 # the class names below are used as the request api directory.  For example: if the class is Circ(db.Model) you can request the database at localhost:5000/api/circ
 
-class Circ(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    sensor= db.Column(db.Integer, nullable=False)
-    # datetime= db.Column(db.DateTime, nullable=True)
+    class Experiments(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        exp_name = db.Column(db.Unicode)
+        box = db.Column(db.Unicode)
+        datetime = db.Column(db.Unicode)
+        correct = db.Column(db.Boolean)
+        testing = db.Column(db.Boolean)
 
-
-class Beeboxes(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    box= db.Column(db.Integer, nullable=False)
-    correct= db.Column(db.Boolean, nullable=False)
-    # datetime= db.Column(db.DateTime, nullable=True)
-    
-db.create_all()
-
-manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Circ, methods= ['GET', 'POST', 'DELETE'], results_per_page=None)    
-manager.create_api(Beeboxes, methods= ['GET', 'POST', 'DELETE'], results_per_page=None)    
-
-app.run()
+    db.create_all()
+    manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
+    manager.create_api(Experiments, methods= ['GET', 'POST'], results_per_page=None)    
+    app.run(host = '0.0.0.0')
